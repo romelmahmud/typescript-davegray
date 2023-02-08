2e013c76d1268d3c109a2549305ddf2c220c7f73
Romel Mahmud <romelmahmud.dev@gmail.com>
2023-02-08 20:57:49 +0600

    lesson 1 complete

---
+++ lesson-1/build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let fullName = 'ayaan';
+console.log(fullName);
+let a = 6;
+let b = 3;
+console.log(a * b);
---
+++ lesson-1/src/main.ts
@@ -0,0 +1,7 @@
+let fullName = 'ayaan'
+console.log(fullName);
+
+let a: number = 6;
+let b: number = 3
+
+console.log(a * b);
